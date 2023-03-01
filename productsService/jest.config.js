10e6136d23e637378ac9b9b2b134f2a6668f3bb9
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2023-03-01 10:52:02 +0300

    [task-4] Update jest config

--- productsService/jest.config.js
+++ productsService/jest.config.js
@@ -1,12 +1,20 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     rootDir: './',
-    preset: 'ts-jest',
     testEnvironment: 'node',
     moduleNameMapper: {
         '^@libs/(.*)$': '<rootDir>/src/libs/$1',
         '^@localtypes/(.*)$': '<rootDir>/src/types/$1',
         '^@models/(.*)$': '<rootDir>/src/models/$1',
         '^@mocks/(.*)$': '<rootDir>/src/mocks/$1'
+    },
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!@middy)'],
+    transform: {
+        '^.+\\.c*[tj]sx?$': [
+            'ts-jest',
+            {
+                babelConfig: true
+            }
+        ]
     }
 };
