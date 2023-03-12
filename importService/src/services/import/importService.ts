cafe199a4dbcca4cefde8ed59287999e5dac7596
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2023-03-12 22:37:31 +0300

    [task-5] Add copying object after streaming

--- importService/src/services/import/importService.ts
+++ importService/src/services/import/importService.ts
@@ -1,5 +1,11 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import {
+    S3Client,
+    PutObjectCommand,
+    GetObjectCommand,
+    CopyObjectCommand,
+    DeleteObjectCommand
+} from '@aws-sdk/client-s3';
 import csv from 'csv-parser';
 import type { BasicS3Config } from '@libs/s3';
 
@@ -26,7 +32,7 @@ export class ImportService {
     async generatePresignedPutUrl(config: BasicS3Config, expiresIn = 3600) {
         const command = new PutObjectCommand({
             Bucket: config.Bucket,
-            Key: `upload/${config.Key}`
+            Key: `uploaded/${config.Key}`
         });
 
         const url = await getSignedUrl(this.client, command, { expiresIn });
@@ -39,11 +45,31 @@ export class ImportService {
 
         const item = await this.client.send(command);
 
-        return new Promise((onResolve) => {
+        return new Promise((onResolve, onReject) => {
             // @ts-ignore
             item.Body.pipe(csv())
-                .on('data', (data) => console.log(`File ${config.Key}, data `, data))
-                .on('end', () => onResolve(true));
+                .on('data', (data) => console.log(`File ${config.Key}, data: `, data))
+                .on('end', async () => {
+                    try {
+                        console.log('Copying file to parsed/ folder...');
+
+                        await this.client.send(
+                            new CopyObjectCommand({
+                                Bucket: config.Bucket,
+                                CopySource: `${config.Bucket}/${config.Key}`,
+                                Key: `parsed/${config.Key.split('/')[1]}`
+                            })
+                        );
+
+                        console.log('Removing file from uploaded/ folder...');
+
+                        await this.client.send(new DeleteObjectCommand(config));
+
+                        onResolve(true);
+                    } catch (e) {
+                        onReject(e);
+                    }
+                });
         });
     }
 }
