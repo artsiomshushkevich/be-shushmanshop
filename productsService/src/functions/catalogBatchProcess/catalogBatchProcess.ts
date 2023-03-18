c385aa9e01ccd1ee6c2900eb75e85ebce3c4a5bf
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2023-03-18 22:45:18 +0300

    [task-6] Add sending sns email notification

--- productsService/src/functions/catalogBatchProcess/catalogBatchProcess.ts
+++ productsService/src/functions/catalogBatchProcess/catalogBatchProcess.ts
@@ -1,9 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 import type { SQSHandler } from 'aws-lambda';
+import { PublishCommand } from '@aws-sdk/client-sns';
 import { DeleteMessageBatchCommand } from '@aws-sdk/client-sqs';
 import { productsService } from '@services/products';
 import { ProductWithCountWithoutId } from '@models/products';
 import { sqsClient } from '@libs/sqs';
+import { snsClient } from '@libs/sns';
 
 export const main: SQSHandler = async (event) => {
     try {
@@ -28,6 +30,15 @@ export const main: SQSHandler = async (event) => {
             new DeleteMessageBatchCommand({
                 QueueUrl: process.env.QUEUE_URL,
                 Entries: event.Records.map((item) => ({ Id: uuidv4(), ReceiptHandle: item.receiptHandle }))
+            })
+        );
+
+        console.log('Sending notification to creators email');
+
+        await snsClient.send(
+            new PublishCommand({
+                TopicArn: process.env.CREATE_PRODUCT_TOPIC_ARN,
+                Message: `Products have been loaded! Date: ${new Date()}`
             })
         );
     } catch (e) {
